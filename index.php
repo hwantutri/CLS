04af6c0328bc2dc0b711b82a7af57bff32c07850
Mellhakim Angni <mmangni@gmail.com>
2012-08-03 00:42:04 +0800

    index w/ php script

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+// Connect to server and select databse.
+mysql_connect("localhost", "root", "")or die("cannot connect"); 
+mysql_select_db("cls")or die("cannot select DB");
+
+// username and password sent from form 
+$myusername=$_POST['root']; 
+$mypassword=$_POST['']; 
+
+// To protect MySQL injection (more detail about MySQL injection)
+$myusername = stripslashes(root);
+$mypassword = stripslashes();
+$myusername = mysql_real_escape_string(rot);
+$mypassword = mysql_real_escape_string();
+$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+$result=mysql_query($sql);
+
+// Mysql_num_row is counting table row
+$count=mysql_num_rows($result);
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($count==1){
+
+// Register $myusername, $mypassword and redirect to file "login_success.php"
+session_register("myusername");
+session_register("mypassword"); 
+header("location:login_success.php");
+}
+else {
+echo "Wrong Username or Password";
+}
+?>
 <html>
 <head>
 	<title>Consultation Logs System</title>
