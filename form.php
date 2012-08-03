46c767874179c04aa3409458bf7fb69deb0430dd
Carl Berondo <xerxes_0805@yahoo.com>
2012-08-03 10:33:07 +0800

    form

---
+++ form.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+<title>Consultation Logs System</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ 	<link rel="stylesheet" type="text/css" href="css/styles.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="css/orbit-1.2.3.css">
+	<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="js/jquery.orbit-1.2.3.min.js"></script>
+	
+    <style type="text/css">
+	body{
+	 //background:#202020;
+	 font:bold 12px Arial, Helvetica, sans-serif;
+	 margin:0;
+	 padding:0;
+	 min-width:960px;
+	 color:#736F6E; 
+	 font-size:20px;
+	 font-style:italic;
+background-color:#ddd;
+}
+input.gray{background-color:#6698FF;
+color:white;
+font-family:"Times New Roman",Georgia,Serif;	
+
+
+
+
+		}
+}
+.container {width: 960px; margin: 0 auto; overflow: hidden;}
+
+
+
+#content {	float: left; width: 100%;}
+
+.post { margin: 0 auto; padding-bottom: 50px; float: left; width: 960px; }
+
+
+</style>
+</script>
+
+<script type="text/javascript">
+
+</script>
+</head>
+<body background = "bg1.jpg">
+<h1>SSS</h1>
+<div class="container">
+	<div id="content">
+    
+		<div class="post">
+			<div id="header"></div>
+			
+			</div>
+				
+				
+				<form name="input" action="" style="text-shadow:4px 4px 8px blue;"align ="left" method="get">
+					Faculty ID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;			<input type="text" name="Fac_id" /><br />
+					Student ID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	<input type="text" name="Student_id" /><br />
+					Date and Time: 	<input type="date" name="Date_and_Time" /><br />
+					Duration: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	<input type="text" name="Duration" /><br />
+					Description:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 	<input type="text" name="Description" /><br />
+					<input class="gray"type="submit" value = "Submit" style="width:150px; height:30px;"/>
+				</form>
+			
+		</div>
+	</div>
+</div>
+
+
+</body>
+</html>
