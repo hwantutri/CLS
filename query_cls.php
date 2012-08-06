52283af83652ce6eea505b60588fc193e65a32a9
Genesis Bacarrisas <genesis.bacarrisas@gmail.com>
2012-08-06 16:24:38 +0800

    add consulation log to database, user sessions

---
+++ query_cls.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+include_once 'database.php';
+$db = new Database();
+
+$uid = $_SESSION['uid_new'];
+$date = $_POST['datepicker'];
+$comment = $_POST['comment'];
+$idnum = $_POST['idno'];
+
+$query = "INSERT INTO consultation (faculty_uid,stud_id,date,description) VALUES ('$uid','$idnum','$date','$comment')";
+mysql_query($query);			
+echo "string";
+
+?>
