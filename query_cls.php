15b403283772521a3d41f1c959e5564d900336c0
hwantutri <genesis.bacarrisas@gmail.com>
2012-08-15 11:59:35 +0800

    Update query_cls.php

--- query_cls.php
+++ query_cls.php
@@ -8,8 +8,9 @@ $uid = $_SESSION['uid_new'];
 $date = $_POST['datepicker'];
 $comment = $_POST['comment'];
 $idnum = $_POST['idno'];
+$subsec = $_POST['subsec'];
 
-$query = "INSERT INTO consultation (faculty_uid,stud_id,date,description) VALUES ('$uid','$idnum','$date','$comment')";
+$query = "INSERT INTO consultation (faculty_uid,stud_id,date,description,subsec) VALUES ('$uid','$idnum','$date','$comment','$subsec')";
 mysql_query($query);			
 echo "string";
 
