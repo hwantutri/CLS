c94037854ecccdd7c8cd4ec631425b140900d739
Carl Berondo <xerxes_0805@yahoo.com>
2012-09-10 00:15:01 +0800

    faculty availability search update
    design

--- form2.php
+++ form2.php
@@ -13,6 +13,8 @@
 	
 <style type="text/css">
 
+body {background-image:url('images/wallpaper3.jpg');
+background-repeat: no-repeat;}
 form legend {
 	color: #2F4F4F;
 	padding: 0 0 20px 0;
@@ -24,6 +26,13 @@ form legend {
 	
 }
 
+h1 {
+  color:#2F4F4F;
+  padding: 0 0 20px 0;
+  font-weight: bold;
+  font-size: 19pt;
+  
+}
 #style {
 	color: #2F4F4F;
 	padding: 0 0 20px 0;
@@ -32,14 +41,27 @@ form legend {
 	font-size: 14px;
 	left: 580px;
 }
-#footer-span {
-	border-top: 1px solid #202020;
-	background:#989898;
-	bottom:0;
-	position:absolute;
+	#footer
+	{
+		width: 1020px;
+		height: 20px;
+		background-repeat: no-repeat;
+	}
+#footer .strong {font-weight: bold; font-size: 16px;}
+#footer p, p:hover, p:visited {color: #2F4F4F;}
+#footer .col1 {float: left; padding: 10px 0 10px 10px; width: 30%; }
+#footer .col2 {float: left; padding: 10px 0 10px ; width: 34%; }
+#footer .col3 {float: left; padding: 10px 0 10px ; width: 34%; }
+#footer {position: relative;
+	margin-top: 80px;
 	height: 80px;
-	width: 100%;
+	clear:both;} 
+#wrap {min-height: 	10%;}
+
+p{color: white;font-size:0.4px;}
+.CSSTableGenerator {width:100%;	box-shadow: 10px 10px 20px #888888;
 }
+
 #mid {
 	background: url(..images/white.png) 0 center no-repeat;
 	text-align: center;
@@ -95,6 +117,7 @@ form legend {
 <body background="bg1.jpg">
 
 <div id="header">
+<img src="images/cls.png" alt="logo" height="44" width="100">
 </div>
 <div id="mid">
 	
@@ -115,10 +138,43 @@ form legend {
 </div>
 </div>
 
-</form>	
+</form>
+<div id="footer">
+<hr />
+	<div class="col1">
+	
+		<p>
+			<span class="strong"><font size="2">Consultation Log System</font></span>
+		</p>
+	</div><!--col1-->
+	
+	<div class="col2">
+		<p>
+		<span class="strong"><font size="2">Contact US:</font></span>		
+		</p >
+		<p>
+		<span><font size="2">0906-139-1950</font></span>
+		<span><font size="2">0905-123-4567</font></span>
+		</p>
+	<p>
+		<span><font size="2">0915-322-4421</font></span>
+		</p>
+		
+	</div><!--col2-->
+	
+	<div class="col3">
+		<p>
+			<span class="strong"><font size="2">Visit US:</font></span>		
+		</p>
+		<p><a href="https://www.facebook.com/"><img src="images/fb.png" alt="facebook" height="25" width="25"></a>
+		<a href="http://twitter.com/"><img src="images/twit.jpg" alt="twitter" height="25" width="25"></a>
+		<a href="https://accounts.google.com/ServiceLogin?service=oz&continue=https://plus.google.com/?gpsrc%3Dgplp0&hl=en"><img src="images/g+.png" alt="google+" height="25" width="25"></a></p>
+	</div><!--col3-->
+</div><!--footer-->
+
+	
 </div>
-<div id = "footer-span">
-</div>
+
 
 </body>
 </html>
